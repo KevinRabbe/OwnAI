7b5ebc0ae03c3ed6132a747834d0918a5e7d7ac2
KevinRabbe <rabbekevin@icloud.com>
2026-05-14 22:08:54 +0200

    Add dependency governance boundary rules

---
+++ packages/dependency-governance/src/boundaryRules.ts
@@ -0,0 +1,19 @@
+export const FORBIDDEN_IMPORT_PATTERNS = [
+  '/src/internal/',
+  '/private/',
+  '/impl/'
+];
+
+export const UI_PACKAGE_PATTERNS = [
+  'apps/dashboard',
+  'ui-core',
+  'ui-holographics',
+  'ui-visualizations'
+];
+
+export const CORE_CONTRACT_PACKAGES = [
+  'core-types',
+  'core-interfaces',
+  'core-events',
+  'core-state'
+];
